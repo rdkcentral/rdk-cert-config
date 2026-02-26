941fab72234084113851e2cff5a31f09f28e949c
“sahmad154” <sohail_ahmad@comcast.com>
2026-02-26 05:18:53 +0000

    Add full Apache 2.0 license headers for copyright scanner

--- test/pkcs11-scripts/setup-pkcs11-openssl.sh
+++ test/pkcs11-scripts/setup-pkcs11-openssl.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
-# Copyright 2026 RDK Management
-# SPDX-License-Identifier: Apache-2.0
+##########################################################################
+# If not stated otherwise in this file or this component's LICENSE file the
+# following copyright and licenses apply:
 #
+# Copyright 2026 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+# SPDX-License-Identifier: Apache-2.0
+##########################################################################
+
 # Setup OpenSSL with PKCS#11 patch for mTLS support
 # This script downloads, patches, and installs OpenSSL 3.0.5
 # Replaces system OpenSSL at runtime when ENABLE_PKCS11=true
--- test/pkcs11-scripts/setup-pkcs11.sh
+++ test/pkcs11-scripts/setup-pkcs11.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 ##########################################################################
+# If not stated otherwise in this file or this component's LICENSE file the
+# following copyright and licenses apply:
+#
+# Copyright 2026 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+# SPDX-License-Identifier: Apache-2.0
+##########################################################################
+
+##########################################################################
 # PKCS#11 Token Setup and Certificate Import
 # 
 # This script combines token initialization and certificate import:
 # 1. Initializes SoftHSM2 token if not exists
 # 2. Imports client certificates to slot 0x01 (standard mTLS)
 # 3. Imports keys to slot 0x02 for PKCS#11 patch testing (production mode)
-#
-# Copyright 2026 RDK Management
-# SPDX-License-Identifier: Apache-2.0
 ##########################################################################
 
 set -e
