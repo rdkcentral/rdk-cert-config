659d3a175f006ee29e994891dea9cf21ef92b24c
“sahmad154” <sohail_ahmad@comcast.com>
2026-02-26 05:44:40 +0000

    Use SPDX-only headers to avoid fossid scanner false positives
    Standard Apache 2.0 license text triggers false matches against external projects.
    Using minimal SPDX format (REUSE compliant) to avoid scanner issues while
    maintaining license compliance.

--- test/pkcs11-scripts/setup-pkcs11-openssl.sh
+++ test/pkcs11-scripts/setup-pkcs11-openssl.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
-##########################################################################
-# Copyright 2026 RDK Management
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# SPDX-FileCopyrightText: 2026 RDK Management
 # SPDX-License-Identifier: Apache-2.0
-##########################################################################
-# Original Work: OpenSSL PKCS#11 patch build system for rdk-cert-config
-# Author: RDK Management
-##########################################################################
+#
 # Setup OpenSSL with PKCS#11 patch for mTLS support
 # This script downloads, patches, and installs OpenSSL 3.0.5
 # Replaces system OpenSSL at runtime when ENABLE_PKCS11=true
